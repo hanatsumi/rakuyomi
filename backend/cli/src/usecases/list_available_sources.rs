6e5c0f8fbc82d23f22fede8b4bacd09c5b2313ba
hanatsumi <hanatsumi@proton.me>
2024-09-25 10:06:25 -0300

    feat: display available/installed sources ordered by name

--- backend/cli/src/usecases/list_available_sources.rs
+++ backend/cli/src/usecases/list_available_sources.rs
@@ -5,7 +5,7 @@ use url::Url;
 use crate::model::SourceInformation;
 
 pub async fn list_available_sources(source_lists: Vec<Url>) -> Result<Vec<SourceInformation>> {
-    let source_informations = stream::iter(source_lists)
+    let mut source_informations: Vec<SourceInformation> = stream::iter(source_lists)
         .then(|source_list| async move {
             anyhow::Ok(
                 reqwest::get(source_list)
@@ -19,6 +19,8 @@ pub async fn list_available_sources(source_lists: Vec<Url>) -> Result<Vec<Source
         .into_iter()
         .flatten()
         .collect();
+
+    source_informations.sort_by_key(|source| source.name.clone());
 
     Ok(source_informations)
 }
--- backend/cli/src/usecases/list_installed_sources.rs
+++ backend/cli/src/usecases/list_installed_sources.rs
@@ -1,9 +1,13 @@
 use crate::{model::SourceInformation, source_collection::SourceCollection};
 
 pub fn list_installed_sources(source_collection: &impl SourceCollection) -> Vec<SourceInformation> {
-    source_collection
+    let mut source_informations: Vec<SourceInformation> = source_collection
         .sources()
         .into_iter()
         .map(|source| source.manifest().into())
-        .collect()
+        .collect();
+
+    source_informations.sort_by_key(|source| source.name.clone());
+
+    source_informations
 }
