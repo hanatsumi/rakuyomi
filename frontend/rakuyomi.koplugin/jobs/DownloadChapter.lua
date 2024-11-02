c15297884d4f0e4d8e8ec2a94ad3969db8bc9575
hanatsumi <hanatsumi@proton.me>
2024-11-02 13:20:40 -0300

    feat: hide downloading chapter dialog if preloaded

--- frontend/rakuyomi.koplugin/jobs/DownloadChapter.lua
+++ frontend/rakuyomi.koplugin/jobs/DownloadChapter.lua
@@ -10,6 +10,7 @@ local JOB_POLLING_INTERVAL_SECONDS = 1
 --- @field private manga_id string
 --- @field private chapter_id string
 --- @field private job_id string
+--- @field private result SuccessfulResponse<string>|ErrorResponse|nil
 local DownloadChapter = {}
 
 --- Creates a new `DownloadChapter` job.
@@ -23,16 +24,14 @@ function DownloadChapter:new(source_id, manga_id, chapter_id)
     source_id = source_id,
     manga_id = manga_id,
     chapter_id = chapter_id,
+    result = nil,
   }
   setmetatable(o, self)
   self.__index = self
 
-  local job_id = o:start()
-  if job_id == nil then
+  if not o:start() then
     return nil
   end
-
-  o.job_id = job_id
 
   return o
 end
@@ -40,45 +39,67 @@ end
 --- Starts the job. Should be called automatically when instantiating a job with `new()`.
 ---
 --- @private
---- @return string|nil id The created job id, or false, if the job could not be created.
+--- @return boolean success Whether the job started successfully.
 function DownloadChapter:start()
   local response = Backend.createDownloadChapterJob(self.source_id, self.manga_id, self.chapter_id)
   if response.type == 'ERROR' then
     logger.error('could not create download chapter job', response.message)
 
-    return nil
+    return false
   end
 
-  return response.body
+  self.job_id = response.body
+
+  return true
 end
 
+--- Peeks the current job competion. Returns `nil` if the job hasn't completed.
+---
+--- @return SuccessfulResponse<string>|ErrorResponse|nil
+function DownloadChapter:poll()
+  if self.result ~= nil then
+    return self.result
+  end
+
+  local response = Backend.getDownloadChapterJobDetails(self.job_id)
+  if response.type == 'ERROR' then
+    self.result = response
+
+    return self.result
+  end
+
+  local details = response.body
+
+  if details.type == 'PENDING' then
+    return nil
+  elseif details.type == 'COMPLETED' then
+    self.result = {
+      type = 'SUCCESS',
+      --- @diagnostic disable-next-line: assign-type-mismatch
+      body = details.data
+    }
+  else
+    self.result = {
+      type = 'ERROR',
+      message = details.data.message
+    }
+  end
+
+  return self.result
+end
+
+--- Runs this job up to completion, returning the result.
+---
 --- @return SuccessfulResponse<string>|ErrorResponse
 function DownloadChapter:runUntilCompletion()
   while true do
-    local response = Backend.getDownloadChapterJobDetails(self.job_id)
-    if response.type == 'ERROR' then
-      return response
+    local result = self:poll()
+
+    if result ~= nil then
+      return result
     end
 
-    local details = response.body
-
-    if details.type == 'PENDING' then
-      ffiutil.sleep(JOB_POLLING_INTERVAL_SECONDS)
-
-      goto continue
-    elseif details.type == 'COMPLETED' then
-      return {
-        type = 'SUCCESS',
-        body = details.data
-      }
-    else
-      return {
-        type = 'ERROR',
-        message = details.data.message
-      }
-    end
-
-    ::continue::
+    ffiutil.sleep(JOB_POLLING_INTERVAL_SECONDS)
   end
 end
 
