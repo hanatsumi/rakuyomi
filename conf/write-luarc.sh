3888b85aca36dc08405e3a0589a1162f1e1837b1
hanatsumi <hanatsumi@proton.me>
2025-01-26 20:31:47 -0300

    chore: add macOS support on devbox

--- conf/write-luarc.sh
+++ conf/write-luarc.sh
@@ -2,14 +2,20 @@ echo "project dir is ${PROJECT_DIR}"
 
 frontendDir="${PROJECT_DIR}/frontend"
 
-koreaderHome=$(dirname $(dirname $(readlink -f $(which koreader))))
+koreaderDerivationOutput=$(dirname $(dirname $(readlink -f $(which koreader))))
+if [[ $OSTYPE == 'darwin'* ]]; then
+  koreaderHome="$koreaderDerivationOutput/Applications/KOReader.app/Contents/koreader"
+else
+  koreaderHome="$koreaderDerivationOutput/lib/koreader"
+fi
+
 cat > "${PROJECT_DIR}/.luarc.json" <<EOF
 {
   "\$schema": "https://raw.githubusercontent.com/sumneko/vscode-lua/master/setting/schema.json",
   "workspace.library": [
     "\${3rd}/luassert/library",
     "\${3rd}/busted/library",
-    "$koreaderHome/lib/koreader/frontend"
+    "$koreaderHome/frontend"
   ],
   "runtime.version": "LuaJIT",
   "diagnostics.neededFileStatus": {
