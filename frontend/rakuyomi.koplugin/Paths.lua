573410bd9c0fc61134edeaae53ede2e1b2d7323e
hanatsumi <hanatsumi@proton.me>
2025-04-16 23:18:49 -0300

    fix: crash caused by wrong plugin path

--- frontend/rakuyomi.koplugin/Paths.lua
+++ frontend/rakuyomi.koplugin/Paths.lua
@@ -12,7 +12,7 @@ end
 
 --- @return string -- The directory in which the plugin is located.
 function Paths.getPluginDirectory()
-  local callerSource = debug.getinfo(2, "S").source
+  local callerSource = debug.getinfo(1, "S").source
   if callerSource:find("^@") then
     local directory, _ = callerSource:gsub("^@(.*)/[^/]*", "%1")
 
